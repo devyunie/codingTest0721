0901dee52c984bbf923e37da14da20effddfdbdb
myeongmoon00 <myha001227@gmail.com>
2024-07-21 13:00:00 +0900

    gitignore add

---
+++ workspace/codingtest/src/codingtest/week01/Ex0101.java
@@ -0,0 +1,23 @@
+package codingtest.week01;
+
+public class Ex0101 {
+
+	public static void main(String[] args) {
+		/*
+		 * 변
+		 */
+		int a = 0;
+		int A = 0;
+		int aA = 0;
+		int _a = 0;
+		int $a = 0;
+		
+		int 1a = 0;
+		int a A = 0;
+		int if = 0;
+		int new = 0;
+		
+		
+	}
+
+}
